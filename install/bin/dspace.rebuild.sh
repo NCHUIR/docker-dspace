aaeb1104b2ad1d74f740e152badba58cf5a86af3
PastLeo <chgu82837@gmail.com>
2016-06-17 18:00:13 +0800

    rebuild script completed but not tested

--- install/bin/dspace.rebuild.sh
+++ install/bin/dspace.rebuild.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-service postgresql start
-dspace.build
-service postgresql stop
+if [ -f $ENV_FILE_PATH ]; then
+    export $(cat $ENV_FILE_PATH);
+fi
+
+rsync --exclude=.git --exclude=*.swp --exclude=*.swo -urv $DSPACE_OUTSIDE_SOURCE_PATH/ $DSPACE_SOURCE_PATH
+dspace.build "-U clean package" "update"
 
