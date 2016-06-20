73e72e76c05bb102e272b406ae2edaa96d464f39
PastLeo <chgu82837@gmail.com>
2016-06-20 18:04:05 +0800

    make rebuild more easizer to ignore profile

--- install/bin/dspace.rebuild.sh
+++ install/bin/dspace.rebuild.sh
@@ -4,6 +4,17 @@ if [ -f $ENV_FILE_PATH ]; then
     export $(cat $ENV_FILE_PATH);
 fi
 
+if [ "$1" ]; then
+    mvn_target="$1"
+else
+    mvn_target="package -P !dspace-lni,!dspace-oai,!dspace-sword,!dspace-swordv2,!dspace-xmlui"
+fi
+if [ "$2" ]; then
+    ant_target="$2"
+else
+    ant_target="update"
+fi
+
 rsync --exclude=.git --exclude=*.swp --exclude=*.swo -urv $DSPACE_OUTSIDE_SOURCE_PATH/ $DSPACE_SOURCE_PATH
-dspace.build "-U clean package" "update"
+dspace.build "$mvn_target" "$ant_target"
 
